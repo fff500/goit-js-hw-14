a8d92755842771d3473bb3074f0ae1b326e049e9
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-10-11 22:58:48 +0300

    Task 1 was added

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+function handleCategories() {
+  const categories = document.getElementsByClassName('item');
+
+  console.log(`Number of categories: ${categories.length}`);
+
+  [...categories].forEach((category) => {
+    const h2 = category.querySelector('h2');
+    const elements = category.querySelectorAll('li');
+
+    console.log(`Category: ${h2.innerText}\nElements: ${elements.length}`);
+  });
+}
+
+handleCategories();
