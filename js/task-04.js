90d3dd9ca4b830c909bc4bb8411680d8ab3d08a6
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-10-15 14:10:10 +0300

    Minor fixes

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,16 @@
-let counter = 0;
+let counterValue = 0;
 const minusButton = document.querySelector('[data-action="decrement"]');
 const plusButton = document.querySelector('[data-action="increment"]');
 const counterSpan = document.getElementById('value');
 
 function decreaseCounter() {
-  counter -= 1;
-  counterSpan.textContent = counter;
+  counterValue -= 1;
+  counterSpan.textContent = counterValue;
 }
 
 function increaseCounter() {
-  counter += 1;
-  counterSpan.textContent = counter;
+  counterValue += 1;
+  counterSpan.textContent = counterValue;
 }
 
 minusButton.addEventListener('click', decreaseCounter);
